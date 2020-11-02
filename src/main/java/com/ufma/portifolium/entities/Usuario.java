9aca5065da00712150599aaac23020914ae0743e
Paulo Gomes <paulo.rgomes22@gmail.com>
2020-11-01 21:00:39 -0300

    fix(cascade tipo usuario): ajuste adicionando o cascade ALL no tipo de usuario na entidade usuario. Quando um tipo de usuário é excluído, também apaga todos os usuarios daquele tipo.

--- src/main/java/com/ufma/portifolium/entities/Usuario.java
+++ src/main/java/com/ufma/portifolium/entities/Usuario.java
@@ -29,7 +29,7 @@ public class Usuario {
     @Column(name="id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="id_tipo")
     private TipoUsuario tipoUsuario;
 
