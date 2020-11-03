89c2a86427dac32e9454fbd03583f98c31859359
Paulo Gomes <paulo.rgomes22@gmail.com>
2020-11-03 18:47:30 -0300

    feat(factory): adicionado um build para usuario com codigo de professor.

--- src/test/java/com/ufma/portifolium/utils/UsuarioFactory.java
+++ src/test/java/com/ufma/portifolium/utils/UsuarioFactory.java
@@ -3,6 +3,7 @@ package com.ufma.portifolium.utils;
 import java.time.LocalDate;
 
 import com.ufma.portifolium.entities.Aluno;
+import com.ufma.portifolium.entities.Professor;
 import com.ufma.portifolium.entities.TipoUsuario;
 import com.ufma.portifolium.entities.Usuario;
 
@@ -16,6 +17,24 @@ public class UsuarioFactory {
         Aluno aluno = AlunoFactory.buildALuno();
         Usuario usuario = Usuario.builder()
                                 .codigoAcesso(aluno.getMatricula())
+                                .criadoEm(data)
+                                .descricao("Um usuario qualquer")
+                                .senha("12345")
+                                .tipoUsuario(tipoUsuario)
+                                .build();
+
+        return usuario;
+    }    
+
+    public static Usuario buildUsuarioProfessor() {
+        LocalDate data = LocalDate.now();
+        TipoUsuario tipoUsuario = TipoUsuario.builder()
+                                            .descricao("Tipo usuario professor teste")
+                                            .build();
+
+        Professor professor = ProfessorFactory.buildProfessor();
+        Usuario usuario = Usuario.builder()
+                                .codigoAcesso(professor.getCodigo())
                                 .criadoEm(data)
                                 .descricao("Um usuario qualquer")
                                 .senha("12345")
