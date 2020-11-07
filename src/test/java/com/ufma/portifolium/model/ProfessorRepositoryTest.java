b88b2196dd1b7e463f9b2eb927dc7475afa45c62
Paulo Ricardo <paulo.rgomes22@gmail.com>
2020-11-07 10:59:50 -0300

    chore(professor repository test): adição do active profile "test".

--- src/test/java/com/ufma/portifolium/model/ProfessorRepositoryTest.java
+++ src/test/java/com/ufma/portifolium/model/ProfessorRepositoryTest.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProfessorRepositoryTest {
     @Autowired
     ProfessorRepository repository;
