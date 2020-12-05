74ad8ad539c068a4a9a86089518550d0f9ffbeab
Paulo Gomes <paulo.rgomes22@gmail.com>
2020-12-05 03:05:55 -0300

    feat(autenticationController)

---
+++ src/main/java/com/ufma/portifolium/controller/AutenticationController.java
@@ -0,0 +1,36 @@
+package com.ufma.portifolium.controller;
+
+import com.ufma.portifolium.model.dto.UsuarioDTO;
+import com.ufma.portifolium.model.exceptions.UsuarioInvalidoException;
+import com.ufma.portifolium.service.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticationController {
+    UsuarioService usuarioService;
+
+    @Autowired
+    public AutenticationController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity realizarLogin(@RequestBody UsuarioDTO usuarioDTO){
+        try {
+            boolean logado = usuarioService.efetuarLogin(usuarioDTO.getLogin(), usuarioDTO.getSenha());
+            if(logado) return new ResponseEntity(true, HttpStatus.OK);
+        } catch (UsuarioInvalidoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
